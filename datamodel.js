e27d05b4fed14a1239cbf802ee7f10ba9aea503c
Mark Masoumi <masoumi.mark@outlook.com>
2023-04-13 11:45:07 -0400

    Added Enemy and Location class to datamodel.js

--- datamodel.js
+++ datamodel.js
@@ -6,12 +6,52 @@ class Hero{
     constructor(){
         const roles = ['Wizard', 'Thief', 'Bard', 'Paladin', 'Sorcerer', 'Mage', 'Ranger', 'Summoner', 'Knight'];
         const personalities = ['Mighty', 'Brave', 'Strong', 'Sly', 'Daring', 'Kind', 'Furious', 'Chaotic', 'Brilliant'];
+        const weapons = ['Sword', 'Knife', 'Staff', 'Wand', 'Mace', 'Spellbook', 'Bow', 'Dagger'];
         this._role = roles[Math.floor(Math.random() * roles.length)];
         this._personality = personalities[Math.floor(Math.random() * personalities.length)];
+        this._weapon = weapons[Math.floor(Math.random() * weapons.length)];
     }
 
     get role(){
         return this._role;
+    }
+
+    get personality(){
+        return this._personality;
+    }
+
+    get weapon(){
+        return this._weapon;
+    }
+}
+
+class Enemy{
+    constructor(){
+        const enemyTypes = ['Goblin', 'Blob', 'Necromancer', 'Troll', 'Snake', 'Bat', 'Rat', 'Beast', 'Demon', 'Skelleton', 'Zombie', 'Ghost'];
+        const personalities = ['Monsterous', 'Evil', 'Slimy', 'Ferocious', 'Horrifying', 'Deadly', 'Frightening', 'Imposing', 'Dreadful'];
+        this._enemyType = enemyTypes[Math.floor(Math.random() * enemyTypes.length)];
+        this._personality = personalities[Math.floor(Math.random() * personalities.length)];
+    }
+
+    get enemyType(){
+        return this._enemyType;
+    }
+
+    get personality(){
+        return this._personality;
+    }
+}
+
+
+class Location{
+    constructor(){
+        const locationTypes = ['Castle', 'Swamp', 'Graveyard', 'Town', 'Mountain', 'Cave', 'Tower', 'Bridge', 'Forest', 'Temple', 'Valley'];
+        this._location = locationTypes[Math.floor(Math.random() * locationTypes.length)];
+        // TODO: location descriptiors / adjectives...
+    }
+
+    get location(){
+        return this._location;
     }
 }
 
